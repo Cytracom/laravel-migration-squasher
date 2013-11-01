c5ca18ce1ddde6826774617cba4266f6b0b521f4
Jacob Barber <JacobA.Barber@yahoo.com>
2013-11-01 15:32:50 -0500

    Formatted automatically generated command file from artisan

--- src/commands/SquashMigrations.php
+++ src/commands/SquashMigrations.php
@@ -2,53 +2,55 @@
 
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 
-class SquashMigrations extends Command {
+class SquashMigrations extends Command
+{
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'migrate:squash';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'migrate:squash';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Command description.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description.';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    /**
+     * Create a new command instance.
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 */
-	public function fire()
-	{
+    /**
+     * Execute the console command.
+     *
+     */
+    public function fire()
+    {
         (new Cytracom\Squasher\MigrationSquasher($this->argument('path')))->squash($this->argument('output'));
-	}
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array(
-            array('path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations folder', app_path() . '/database/migrations'),
-            array('output', null, InputOption::VALUE_OPTIONAL, 'The path to the output folder of squashes', app_path() . '/database/migrations')
-		);
-	}
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('path', null, InputOption::VALUE_OPTIONAL, 'The path to the migrations folder',
+                app_path() . '/database/migrations'),
+            array('output', null, InputOption::VALUE_OPTIONAL, 'The path to the output folder of squashes',
+                app_path() . '/database/migrations')
+        );
+    }
 
 }
